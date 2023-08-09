8afaf3d3871b92fb02537b6c331c66e89a21f865
Wei-Lun Chen <renchen8394@gmail.com>
2023-08-09 12:03:14 +0800

    new script

--- run.sh
+++ run.sh
@@ -1,19 +1,13 @@
 export CUDA_VISIBLE_DEVICES='0'
 
-python main.py --dataset FallAllD --model CNN --epochs 100
-
-python main.py --dataset SisFall --model CNN --epochs 100
+python main.py --dataset FallAllD --model CNN --epochs 100 --location Wrist
  
-python main.py --dataset UMAFall --model CNN --epochs 100
+python main.py --dataset UMAFall --model CNN --epochs 100 --location Wrist
 
-python main.py --dataset FallAllD --model LSTM --epochs 250
-
-python main.py --dataset SisFall --model LSTM --epochs 250
+python main.py --dataset FallAllD --model LSTM --epochs 200 --location Wrist
  
-python main.py --dataset UMAFall --model LSTM --epochs 250
+python main.py --dataset UMAFall --model LSTM --epochs 200 --location Wrist
 
-python main.py --dataset FallAllD --model CNNLSTM --epochs 250
-
-python main.py --dataset SisFall --model CNNLSTM --epochs 250
+python main.py --dataset FallAllD --model CNNLSTM --epochs 150 --location Wrist
  
-python main.py --dataset UMAFall --model CNNLSTM --epochs 250
+python main.py --dataset UMAFall --model CNNLSTM --epochs 150 --location Wrist
