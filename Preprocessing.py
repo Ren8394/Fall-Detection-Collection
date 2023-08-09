012db6862bb3b82c2ac1bfc29a11ac775a4b49a8
Wei-Lun Chen <renchen8394@gmail.com>
2023-08-09 12:02:56 +0800

    adjust for dataset features

--- Preprocessing.py
+++ Preprocessing.py
@@ -68,14 +68,18 @@ def preprocessing_UMAFall(loadfile_path, savefile_path, sensor, location, sampli
         raise ValueError('sensor must be Acc or Gyr, but got {}'.format(sensor))
 
     # drop unused rows
-    if location == ['Waist', 'Wrist']:
+    if location == ['Waist', 'Wrist', 'Right Pocket', 'Ankle']:
         pass
     elif location == ['Waist']:
         df = df[df['Device'] == 'Waist']
     elif location == ['Wrist']:
         df = df[df['Device'] == 'Wrist']
+    elif location == ['Right Pocket']:
+        df = df[df['Device'] == 'Right Pocket']
+    elif location == ['Ankle']:
+        df = df[df['Device'] == 'Ankle']
     else:
-        raise ValueError('location must be Waist or Wrist, but got {}'.format(location))
+        raise ValueError('location must be Waist, Wrist, Right Pocket, and Ankle, but got {}'.format(location))
 
     # resampling (Original sampling rate = 20Hz)
     df['Acc'] = df['Acc'].apply(lambda x: resampling(x, 20, sampling_rate, duration))
@@ -103,14 +107,10 @@ def preprocessing_SisFall(loadfile_path, savefile_path, sensor, location, sampli
         raise ValueError('sensor must be Acc or Gyr, but got {}'.format(sensor))
 
     # drop unused rows
-    if location == ['Waist', 'Wrist']:
-        pass
-    elif location == ['Waist']:
+    if location == ['Waist']:
         df = df[df['Device'] == 'Waist']
-    elif location == ['Wrist']:
-        df = df[df['Device'] == 'Wrist']
     else:
-        raise ValueError('location must be Waist or Wrist, but got {}'.format(location))
+        raise ValueError('location must be Waist, but got {}'.format(location))
 
     # resampling (Original sampling rate = 200Hz)
     df['Acc'] = df['Acc'].apply(lambda x: resampling(x, 200, sampling_rate, duration))
