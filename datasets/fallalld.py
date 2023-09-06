f69ac3941001345cfe052f8c2d5f6fee8a53deb2
Ren <renchen8394@gmail.com>
2023-09-06 12:03:42 +0800

    fallalld init

--- datasets/fallalld.py
+++ datasets/fallalld.py
@@ -1,13 +1,23 @@
 import numpy as np
 import pandas as pd
 
-from typing import Tuple, Any
+from pathlib import Path
 from torch.utils.data import Dataset
+from typing import Any, Callable, Dict, List, Optional, Tuple
 
-class fallalld(Dataset):
 
-    def __init__(self) -> None:
-        super().__init__()
+class FallAllD(Dataset):
+    
+    base_folder = Path.cwd().joinpath("TMP")
+    _RESOURCES = {
+        "train": ("FallAllD_train.pkl", "202308311431"),
+        "val": ("FallAllD_val.pkl", "202308311431"),
+        "test": ("FallAllD_test.pkl", "202308311431")
+    }
+
+
+    def __init__(self, split: str = "train", download: bool = False) -> None:
+        pass
 
     def __len__(self) -> int:
         pass
